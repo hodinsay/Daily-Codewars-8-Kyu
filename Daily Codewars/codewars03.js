df4d436ec40c2e8c6d239da7649fe41b59c4085c
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-03-24 22:02:55 -0700

    Add files via upload

---
+++ Daily Codewars/codewars03.js
@@ -0,0 +1,13 @@
+// Reversed Strings
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
+  }
