3ba6998b48c3975460afd182a66ca8d791ebc3d6
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-04-07 20:30:27 -0700

    Add files via upload

---
+++ Daily Codewars/codewars23.js
@@ -0,0 +1,11 @@
+// Removing customElements
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+const removeEveryOther = array => array.filter((_, i) => i % 2 === 0)
