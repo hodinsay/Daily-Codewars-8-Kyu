59e9a16747ee63535675530b23197d14ac1fd481
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-04-09 14:37:55 -0700

    Add files via upload

---
+++ Daily Codewars/codewars25.js
@@ -0,0 +1,9 @@
+// Name Shuffler
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+function nameSuffle(str){
+    return str.split(' ').reverse().join(' ');
+  }
