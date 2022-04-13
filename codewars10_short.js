52576b9f07f3d461c24bd6ff21ce87a9f40b3f14
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-04-12 23:39:52 -0700

    Add files via upload

---
+++ codewars10_short.js
@@ -0,0 +1,8 @@
+//String Repeat_Short
+
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+const repeatStr = (n, s) => s.repeat(n);
