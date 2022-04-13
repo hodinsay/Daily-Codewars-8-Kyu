b5dc3ffc11056cc27ae162767069dd4ed9985777
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-04-13 15:03:37 -0700

    Delete codewars09_short.js

--- codewars09_short.js
+++
@@ -1,5 +0,0 @@
-// Even or Odd_Short
-
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-
-const even_or_odd = number => number % 2 === 0 ? "Even" : "Odd";
