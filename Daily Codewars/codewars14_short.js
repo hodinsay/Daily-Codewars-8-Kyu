29eed2b87f3e25d644c0e41b44f2cb30b083d076
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-04-13 15:04:51 -0700

    Add files via upload

---
+++ Daily Codewars/codewars14_short.js
@@ -0,0 +1,10 @@
+// Convert a string to an array_Short
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input -> Output):
+// * "Robin Singh" ==> ["Robin", "Singh"]
+
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+const stringToArray = string => string.split(" ");
