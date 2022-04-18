4edde75b9fbd4be172331b9498e549bdaf5c3a08
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-04-18 14:38:47 -0700

    Add files via upload

---
+++ Daily Codewars/codewars33.js
@@ -0,0 +1,18 @@
+// Returning Strings
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// Solution 1: 
+
+function greet(name){
+    //your code here
+    return "Hello, " + name + " how are you doing today?";
+  }
+
+// Solution 2:
+function greet(name){
+    //your code here
+    return `Hello, ${name} how are you doing today?`;
+  }
---
+++ Daily Codewars/codewars33_short.js
@@ -0,0 +1,13 @@
+// Returning Strings_short
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// Solution 1: 
+
+const greet = name => "Hello, " + name + " how are you doing today?";
+
+// Solution 2:
+
+const greet = name => `Hello, ${name} how are you doing today?`;
