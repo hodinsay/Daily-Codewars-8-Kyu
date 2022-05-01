1ff2910b503bceaecc644b2c0db21706a79f07f7
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-04-30 23:16:10 -0700

    Add files via upload

---
+++ Daily Codewars/codewars39.js
@@ -0,0 +1,9 @@
+// Binary to Decimal
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// Solution:
+
+function binToDec(bin){
+    return parseInt(bin,2);
+  }
