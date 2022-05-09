9606425172e96e45f28f612748dcaf9f1738d90a
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-05-08 23:39:19 -0700

    Add files via upload

---
+++ Daily Codewars/codewars46.js
@@ -0,0 +1,9 @@
+// Remove String Spaces
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// Solution: 
+
+function noSpace(x){
+    return x.replace(/\s/g, '');
+  }
