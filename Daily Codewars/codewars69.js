d2daf2df384ee1cc3ca2e1cdd422b97964de499d
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-05-18 23:29:47 -0700

    Add files via upload

---
+++ Daily Codewars/codewars69.js
@@ -0,0 +1,30 @@
+// Count the Monkeys!
+
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+// For example:
+
+// monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// monkeyCount(1) // --> [1]
+
+// Solution 1:
+
+function monkeyCount(n) {
+    let monkeys = [];
+    for(let i=1; i<n+1; i++){
+     monkeys.push(i);
+    }
+    return monkeys;
+  }
+
+// Solution 2:
+
+const monkeyCount = (n) => [...Array(n)].map((_, i) => i +1);
+
+// Solution 3:
+
+function monkeyCount(n) {
+    return [...Array(n)].map((_, i) => i +1);
+  }
