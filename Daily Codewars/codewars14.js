ea181ff8769bbf1bf751eb3fa2d4d98cf9f55b0a
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-06-02 16:36:08 -0700

    Add files via upload

--- Daily Codewars/codewars14.js
+++ Daily Codewars/codewars14.js
@@ -1,12 +1,48 @@
-// Convert a string to an array
+// Complementary DNA
 
-// Write a function to split a string and convert it into an array of words.
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
-// Examples (Input -> Output):
-// * "Robin Singh" ==> ["Robin", "Singh"]
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
 
-// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
-function stringToArray(string){
-    return string.split(" ")
-}
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+// Solution 1: 
+
+function DNAStrand(dna) {
+    return dna.replace(/./g, function(c) {
+      return DNAStrand.pairs[c]
+    })
+  }
+  
+  DNAStrand.pairs = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+  }
+
+// Solution 2:
+
+function DNAStrand(dna){
+    let str = '';
+    
+    for (let i = 0; i <= dna.length; i++){
+      if(dna[i] === 'A'){
+        str += 'T';
+      } else if (dna[i] === 'T'){
+          str += 'A';
+      } else if (dna[i] === 'G'){
+          str += 'C';
+      } else if (dna[i] === 'C'){
+          str += 'G';
+      }
+    }
+      return str;
+  }
